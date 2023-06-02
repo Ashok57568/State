a8f1f8e8136b1b2a8929a84dc3580df6722884a4
Ashok Dhungana <110181629+Ashok57568@users.noreply.github.com>
2023-06-02 13:44:23 +0545

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+  var isDone = true;
+
+  const strikeThrough = { textDecoration: "line-through" };
+
+  return <p style={isDone ? strikeThrough : null}>Buy Milk</p>;
+}
+export default App;
